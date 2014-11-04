0652695f30209c77e93c1835569e528292273128
Diego Romero Armijos <diegoacuario11@gmail.com>
2014-11-04 00:13:52 -0500

    Update ingresolaboratoriodb.sql
    Actualizar script sql

--- sql/ingresolaboratoriodb.sql
+++ sql/ingresolaboratoriodb.sql
@@ -27,8 +27,8 @@ DROP TABLE IF EXISTS `equipos`;
 CREATE TABLE `equipos` (
   `id_equipo` int(11) NOT NULL AUTO_INCREMENT,
   `id_laboratorio` int(11) NOT NULL,
-  `ip` varchar(45) CHARACTER SET latin1 NOT NULL,
-  `mac` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `ip` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `mac` varchar(12) CHARACTER SET latin1 NOT NULL,
   `numero` int(11) NOT NULL COMMENT 'Número de equipo en el laboratorio',
   `estado` int(11) NOT NULL COMMENT '0->Disponible\n1->Libre\n2->Ocupado',
   PRIMARY KEY (`id_equipo`),
@@ -36,7 +36,7 @@ CREATE TABLE `equipos` (
   UNIQUE KEY `ip_UNIQUE` (`ip`),
   KEY `equipoLaboratorio_idx` (`id_laboratorio`),
   CONSTRAINT `equipoLaboratorio` FOREIGN KEY (`id_laboratorio`) REFERENCES `laboratorios` (`id_laboratorio`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `equipos` (
 
 LOCK TABLES `equipos` WRITE;
 /*!40000 ALTER TABLE `equipos` DISABLE KEYS */;
-INSERT INTO `equipos` VALUES (2,1,'192.168.10.9',' B8975A5C02B9',9,2);
+INSERT INTO `equipos` VALUES (2,1,'192.168.10.9',' B8975A5C02B',9,2),(3,1,'192.168.10.10','ABABAC265663',10,0);
 /*!40000 ALTER TABLE `equipos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ DROP TABLE IF EXISTS `laboratorios`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `laboratorios` (
   `id_laboratorio` int(11) NOT NULL AUTO_INCREMENT,
-  `codigo` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `codigo` varchar(10) CHARACTER SET latin1 NOT NULL,
   `nombre` varchar(45) CHARACTER SET latin1 NOT NULL,
   `descripcion` varchar(255) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id_laboratorio`),
@@ -73,7 +73,7 @@ CREATE TABLE `laboratorios` (
 
 LOCK TABLES `laboratorios` WRITE;
 /*!40000 ALTER TABLE `laboratorios` DISABLE KEYS */;
-INSERT INTO `laboratorios` VALUES (1,'UNL01','Ingeniería de sistemas','Laboratorio para estudiantes de la carrera en sistemas infórmaticos y computación');
+INSERT INTO `laboratorios` VALUES (1,'UNL01','Ingenieria de sistemas','Laboratorio para estudiantes \nde la carrera en sistemas infrmaticos y computacin');
 /*!40000 ALTER TABLE `laboratorios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `sesiones` (
   KEY `usuarioEquipo_idx` (`id_usuario`),
   CONSTRAINT `sesionEquipo` FOREIGN KEY (`id_equipo`) REFERENCES `equipos` (`id_equipo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `sesionUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +105,7 @@ CREATE TABLE `sesiones` (
 
 LOCK TABLES `sesiones` WRITE;
 /*!40000 ALTER TABLE `sesiones` DISABLE KEYS */;
-INSERT INTO `sesiones` VALUES (2,2,1,1,'2014-11-03 20:53:08','2014-11-03 20:53:14'),(3,2,1,1,'2014-11-03 20:53:29','2014-11-03 21:01:35'),(4,2,1,1,'2014-11-03 20:55:05','2014-11-03 20:55:12'),(5,2,1,1,'2014-11-03 21:01:47','2014-11-03 21:03:26');
+INSERT INTO `sesiones` VALUES (2,2,1,1,'2014-11-03 20:53:08','2014-11-03 20:53:14'),(3,2,1,1,'2014-11-03 20:53:29','2014-11-03 21:01:35'),(4,2,1,1,'2014-11-03 20:55:05','2014-11-03 20:55:12'),(5,2,1,1,'2014-11-03 21:01:47','2014-11-03 21:03:26'),(6,3,1,1,'2014-11-03 22:27:10','2014-11-03 22:27:13');
 /*!40000 ALTER TABLE `sesiones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,4 +151,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-11-03 17:18:20
+-- Dump completed on 2014-11-03 17:56:20
